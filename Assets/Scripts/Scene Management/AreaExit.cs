29e6acec7cbbb5195917ed52c1d2ef89e7da88b3
Deni4kaHun7 <denis.serkes@gmail.com>
2023-12-09 16:12:50 +0100

    added singleton

---
+++ Assets/Scripts/Scene Management/AreaExit.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AreaExit : MonoBehaviour
+{
+    [SerializeField] private string nextScene;
+    private void OnTriggerEnter2D(Collider2D other) {
+        SceneManager.LoadScene(nextScene);
+    }
+}
